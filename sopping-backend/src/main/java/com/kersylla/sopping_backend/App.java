3f410230be4fec2c7a16f0764dfe9f6258b891aa
kersylla <kersylla92@gmail.com>
2017-08-28 22:31:55 +0200

    First Commit Online Shopping

---
+++ sopping-backend/src/main/java/com/kersylla/sopping_backend/App.java
@@ -0,0 +1,13 @@
+package com.kersylla.sopping_backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
